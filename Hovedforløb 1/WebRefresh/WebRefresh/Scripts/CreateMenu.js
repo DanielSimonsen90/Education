d392f270a14ca1af4f11a0dea7511ce7b5d55b7d
DanielSimonsen90 <danielsimonsen90@gmail.com>
2020-10-30 10:07:22 +0100

    Webrefresh

---
+++ Hovedforløb 1/WebRefresh/WebRefresh/Scripts/CreateMenu.js
@@ -0,0 +1,23 @@
+﻿function Start() {
+    $("#header").load("Layout/Header.html");
+    $("#footer").load("../../Webpages/Layout/Footer.html");
+
+    var CurrentPage;
+
+    try { CurrentPage = document.location.href.match(/[^\/]+$/)[0]; }
+    catch { CurrentPage = "index.html"; }
+    finally {
+        setTimeout(function () {
+            PageLoadTimeOut(CurrentPage);
+        }, 200);
+    }
+}
+
+function PageLoadTimeOut(CurrentPageInfo) {
+    $("header a").each($(this).removeClass("VisitPage"));
+
+    $("header").find('a').each(function () {
+        if ($(this).attr('href') == CurrentPageInfo)
+            $(this).addClass("VisitPage");
+    });
+}
