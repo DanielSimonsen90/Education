d92f2d783ff8603f694f20771a17715d66a751c2
DanielSimonsen90 <56531250+DanielSimonsen90@users.noreply.github.com>
2021-05-03 07:15:12 +0200

    Happy birthday part 1

---
+++ Hovedforløb 3/Serverside/The Nice Guy Community/NGC_MVC/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NGC_Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NGC_MVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public Login CreateLogin(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+        public bool Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
