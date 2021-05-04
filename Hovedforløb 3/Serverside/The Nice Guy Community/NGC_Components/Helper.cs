ecb0116e52e077482756a72aaab637e9fc290684
DanielSimonsen90 <56531250+DanielSimonsen90@users.noreply.github.com>
2021-05-04 16:11:33 +0200

    How to get response from database

---
+++ Hovedforløb 3/Serverside/The Nice Guy Community/NGC_Components/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGC_Components
+{
+    public static class Helper
+    {
+        public static async Task<string> GetData(int port, string path)
+        {
+            //using HttpClient client = new();
+            //HttpResponseMessage res = await client.GetAsync(url);
+            //HttpContent content = res.Content;
+            //string data = await content.ReadAsStringAsync();
+            //return data;
+
+            string LoginsFromApiURI = $"https://localhost:{port}/api/{path}";
+            return await new HttpClient().GetStringAsync(LoginsFromApiURI);
+        }
+    }
+}
--- Hovedforløb 3/Serverside/The Nice Guy Community/NGC_MVC/Controllers/LoginController.cs
+++ Hovedforløb 3/Serverside/The Nice Guy Community/NGC_MVC/Controllers/LoginController.cs
@@ -5,15 +5,50 @@ using System.Collections.Generic;
 using NGC_API;
 using System.Threading.Tasks;
 using NGC_API.Repositories;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
 
 namespace NGC_MVC.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly ILogger<LoginController> _logger;
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+            OnLoginAttempt += OnLoginAttempted;
+            OnLoginSuccessful += OnLoginSucceeded;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        public delegate Task<Login> LoginAttempt(string username, string password);
+        public event LoginAttempt OnLoginAttempt;
+        private async Task<Login> OnLoginAttempted(string username, string password)
+        {
+            string LoginsFromApiURI = "https://localhost:44327/api/Login";
+
+            string loginsRes = await new HttpClient().GetStringAsync(LoginsFromApiURI);
+            IList<Login> logins = new List<Login>() { new Login("admin", "admin") };
+            if (logins == null || logins.Count == 0) throw new Exception("No logins found!");
+
+            Login loginFromLogins = (logins as List<Login>).Find(l => l.Username == username && l.Password == password);
+            string loginRes = await Helper.GetData(44327, $"Logins/{loginFromLogins.ID}");
+
+            if (loginFromLogins != null) OnLoginSuccessful.Invoke(loginFromLogins);
+            return loginFromLogins;
+        }
+
+        public delegate void LoginSuccessful(Login login);
+        public event LoginSuccessful OnLoginSuccessful;
+        private void OnLoginSucceeded(Login login)
+        {
+            _logger.LogInformation($"{login.Username} successfully logged in", login);
+
+        }
+
 
         public async Task<Login> CreateLogin(string username, string password)
         {
@@ -22,15 +57,8 @@ namespace NGC_MVC.Controllers
             //    APIUserController.Post(user),
             //    APIController.Post(user.Login)
             //);
-            return user.Login;
+            return await OnLoginAttempt.Invoke(username, password);
         }
-        public async Task<Login> Login(string username, string password)
-        {
-            //IList<Login> logins = await APIController.Get();
-            IList<Login> logins = new List<Login>() { new Login("admin", "admin") };
-            if (logins == null || logins.Count == 0) throw new Exception("No logins found!");
-
-            return (logins as List<Login>).Find(l => l.Username == username && l.Password == password);
-        }
+        public async Task<Login> Login(string username, string password) => await OnLoginAttempt.Invoke(username, password);
     }
 }
