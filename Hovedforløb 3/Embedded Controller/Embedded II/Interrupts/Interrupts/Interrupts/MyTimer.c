610bd5183d26bbb8f301aa4adca641f354c37679
DanielSimonsen90 <danielsimonsen90@gmail.com>
2021-06-14 14:00:09 +0200

    14/6

--- Hovedforløb 3/Embedded Controller/Embedded II/Interrupts/Interrupts/Interrupts/MyTimer.c
+++ Hovedforløb 3/Embedded Controller/Embedded II/Interrupts/Interrupts/Interrupts/MyTimer.c
@@ -9,34 +9,28 @@
 #include "MyTimer.h"
 
 uint32_t Counter = 0;
+#define LED PB0 //digitalPin 8
 
 void SetupTimer() {
-	printf("Setting up timer\n");
 	TCCR1B |= (1 << CS12); // Select pre-scaler 256, used to divide with the clock frequence
 	OCR1A |= 0xF424; //Value used to match timer value (i.e. Timer.Value == 1s) where the 1s is this value
 	TCCR1B |= (1 << WGM12); //Restart interrupt timer
 	
+	DDRB |= (1 << DDB0);
+	
 	EnableTimer();
-	printf("Timer sat up\n");
 }
 void EnableTimer() {
-	printf("Enabling timer\n");
-	//UCSR0B |= (1 << RXCIE0);
 	TIMSK1 |= (1 << OCIE1A); //Turn on Output Compare A
-	printf("Timer enabled\n");
-	
 }
 void DisableTimer() {
-	printf("Disabling timer\n");
-	
-	
-	
-	printf("Timer disabled\n");
+	TIMSK1 &= ~(1 << OCIE1A); //Turn off Output Compare A
 }
 
 ISR(TIMER1_COMPA_vect) {
 	Counter++;
 	printf("\n%ld", Counter);
+	LED ^= (1 << PB0);
 	
 	sei();
 }
