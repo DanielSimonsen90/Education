203391098026241d4257ca0e6404b0dbf5607bc7
DanielSimonsen90 <danielsimonsen90@gmail.com>
2021-11-04 14:49:12 +0100

    python difficult

--- Hovedforløb 4/Machine Learning/main.py
+++ Hovedforløb 4/Machine Learning/main.py
@@ -1,6 +1,50 @@
 import pandas as panda
 
-data = panda.read_csv("smsspamcollection~/SMSSpamCollection.csv", sep="\n")
-hams = data.filter(items=)
 
-print(data)
+data = panda.read_csv("smsspamcollection/SMSSpamCollection.csv", sep=",")
+
+
+def run_file_conversion():
+    data_frame = panda.read_csv(filepath_or_buffer="smsspamcollection/SMSSpamCollection", sep="\t", names=["Category", "Text"])
+    data_frame.to_csv(path_or_buf="smsspamcollection/SMSSpamCollection.csv", sep=",", index=False)
+
+
+def filter_data_by_spam_type(spam_type): return data[data["Category"] == spam_type]
+
+
+def get_percentage(spam_type): return str(int(filter_data_by_spam_type(spam_type).size / data.size * 100))
+
+
+def combine_equally():
+    ham = filter_data_by_spam_type("ham")
+    spam = filter_data_by_spam_type("spam")
+
+    minimum = min([ham.shape[0], spam.shape[0]])
+
+    ham_cut = ham.sample(minimum)
+    spam_cut = spam.sample(minimum)
+
+    combined = panda.concat([spam_cut, ham_cut])
+
+    print(
+        f"Ham Size: {ham_cut.shape[0]}\n"
+        f"Spam Size: {spam_cut.shape[0]}\n"
+        f"Minimum: {minimum}"
+    )
+
+
+def get_percentage_between_data():
+    percent_ham = get_percentage("ham")
+    percent_spam = get_percentage("spam")
+    total = str(int(percent_ham) + int(percent_spam))
+    print(
+        f"Ham Percent: {percent_ham}%\n" +
+        f"Spam Percent: {percent_spam}%\n" +
+        f"Total Percent: {total}%"
+    )
+
+
+if __name__ == '__main__':
+    #run_file_conversion()
+    get_percentage_between_data()
+    combine_equally()
