512ef1480675cb39ddb670bd182cb3969cc6574c
Daniel Simonsen <danielsimonsen90@gmail.com>
2022-06-01 13:59:29 +0200

    All of this looks like it's useless lmao

--- Hovedforløb 3/Database programmering/Final Assignment/DanhosMessages/DanhosMessages/obj/Debug/net5.0-windows/App.g.i.cs
+++ Hovedforløb 3/Database programmering/Final Assignment/DanhosMessages/DanhosMessages/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace DanhosMessages {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace DanhosMessages {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
         public static void Main() {
             DanhosMessages.App app = new DanhosMessages.App();
             app.InitializeComponent();
